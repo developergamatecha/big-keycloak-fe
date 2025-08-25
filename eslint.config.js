57da3d55c9ac09aa85092dc4ee22ccab5ff1aa72
Farih Akmal Haqiqi <akmal@gamatecha.com>
2025-08-25 11:49:05 +0700

    setup: add keycloakify-react with tailwind

---
+++ eslint.config.js
@@ -0,0 +1,51 @@
+import js from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import storybook from 'eslint-plugin-storybook';
+import globals from 'globals';
+import typescriptEslint from 'typescript-eslint';
+
+export default typescriptEslint.config(
+  js.configs.recommended,
+  ...typescriptEslint.configs.recommended,
+  react.configs.flat.recommended,
+  react.configs.flat['jsx-runtime'],
+  eslintConfigPrettier,
+  ...storybook.configs['flat/recommended'],
+  {
+    ignores: ['dist/**', 'public/**'],
+  },
+  {
+    plugins: {
+      'react-refresh': reactRefresh,
+      'react-hooks': reactHooks,
+    },
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    rules: {
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      'react-hooks/exhaustive-deps': 'off',
+      '@typescript-eslint/no-redeclare': 'off',
+      'no-labels': 'off',
+    },
+  },
+  {
+    files: ['**/*.stories.*'],
+    rules: {
+      'import/no-anonymous-default-export': 'off',
+    },
+  },
+);
---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import react from '@vitejs/plugin-react';
+import { keycloakify } from 'keycloakify/vite-plugin';
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    keycloakify({
+      accountThemeImplementation: 'none',
+    }),
+  ],
+});
